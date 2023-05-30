ea4279f02bd73eeea6e74419a1bf9929d38ca8f1
André Gouveia <49957243+andregomex10@users.noreply.github.com>
2023-05-31 00:23:51 +0100

    tes

--- Models/Domain/Competition.cs
+++ Models/Domain/Competition.cs
@@ -1,46 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using System.ComponentModel.DataAnnotations;
 
 namespace cineVote.Models.Domain
 {
-    [Table("tblCompetition")]
     public class Competition
     {
         [Key]
-        [Column("Competition_Id")] // Use the exact column name from the database
-        public int Competition_Id { get; set; }
-
-        [Column("Name")]
-        public string? Name { get; set; }
-
-        [Column("IsPublic")]
-        public bool IsPublic { get; set; }
-
-        [Column("StartDate")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string imageURL { get; set; }
         public DateTime StartDate { get; set; }
-
-        [Column("EndDate")]
         public DateTime EndDate { get; set; }
+        public bool IsOpen { get; set; }
 
-        [ForeignKey("CategoryId")]
-        [Column("CategoryId")]
-        public int CategoryId { get; set; }
+        //Relatioships
 
-        [ForeignKey("AdminId")]
-        [Column("AdminId")]
-        public string AdminId { get; set; }
+        public List<Nominee> Nomines { get; set; }
 
-        [Column("CategoryEntity")]
-        public ICollection<Category> Categories { get; set; }
-
-        [Column("NomineeCompetitions")]
-        public List<Nominee> Nominees { get; set; }
-
-        [Column("Results")]
-        public Result? Results { get; set; }
-
-
-        [NotMapped]
-        public List<Dictionary<string, object>> Movies { get; set; }
     }
+
 }
